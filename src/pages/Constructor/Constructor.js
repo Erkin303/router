dcb7f6eefeacdb9993a854e09f74eacd6411c684
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-07-29 09:10:45 +0300

    biron bir ozgartirishni yozish

--- src/pages/Constructor/Constructor.js
+++ src/pages/Constructor/Constructor.js
@@ -6,7 +6,42 @@ import React from 'react'
 export const Constructor = () => {
     return (
         <div>
-            <h1 className='text-center text-success'>Constructor page</h1>
+            <div className="section">
+                <div className="container">
+                    <p className='kattaHarf'>Constructor</p>
+                    <p className='kichiikHarf'>Крупная лифтостроительная компания в Узбекистане!</p>
+                </div>
+
+            </div>
+
+            <footer>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6 col-sm12">
+                            <div className='footers'>
+                                <p className='fs-4'>О НАС</p>
+                                <p>«Osten» — уникальный производитель, лифтов и эскалаторов в Средней Азии Компания «Osten» была основана в 2019 году!</p>
+                                <p className='fs-4'>МЫ В СОЦ.СЕТЯХ:</p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm12">
+                            <div className='footers'>
+                                <p><a href="#">Построен новый лифт</a></p>
+                                <p><a href="#">РЕМОНТ И ОБСЛУЖИВАНИЕ ЭСКАЛАТОРОВ</a></p>
+                                <p><a href="#">ДИСПЕТЧЕРИЗАЦИЯ</a></p>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 col-sm12">
+                            <div className='footers'>
+                                <p className='fs-4'>КОНТАКТЫ</p>
+                                <p>Номер телефона:  +998 (93) 511-11-17; +998 (91) 791-51-15</p>
+                                <p>Электронная почта: <a href="#">info@uzlift.uz</a></p>
+                                <p >Адрес:  100077, город Ташкент, улица Мухаммад Юсуф 1А.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
 
         </div>
     )
